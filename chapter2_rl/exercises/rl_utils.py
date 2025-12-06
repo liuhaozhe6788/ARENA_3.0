32a1b3bc346d0507e6ffd6e32248556f304de2ed
liuhaozhe6788 <48321861+liuhaozhe6788@users.noreply.github.com>
2025-12-06 18:08:12 +0100

    Disable FrameStack in environment setup
    Comment out FrameStack wrapper in rl_utils.py

--- chapter2_rl/exercises/rl_utils.py
+++ chapter2_rl/exercises/rl_utils.py
@@ -5,7 +5,6 @@ from tqdm import tqdm
 import numpy as np
 from IPython.display import HTML
 
-from gymnasium.wrappers.frame_stack import FrameStack
 from gymnasium.wrappers.transform_observation import TransformObservation, ResizeObservation, GrayScaleObservation, NormalizeObservation
 from gymnasium.wrappers.transform_action import ClipAction
 from gymnasium.wrappers.transform_reward import TransformReward, NormalizeReward
@@ -57,7 +56,7 @@ def prepare_atari_env(env: gym.Env):
     env = ClipRewardEnv(env)
     env = ResizeObservation(env, shape=(84, 84))
     env = GrayScaleObservation(env)
-    env = FrameStack(env, num_stack=4)
+    # env = FrameStack(env, num_stack=4)
     return env
 
 
